0a38f3f8027991ceaa8d6eb6cfb2d698d1c59da5
SMCRules <magbelmonte@googlemail.com>
2026-05-04 20:04:54 +0100

    information gain with binary and multiclass categorical features

--- trees/id3_primer.py
+++ trees/id3_primer.py
@@ -13,13 +13,78 @@ def entropy(var):
     probs = var.value_counts(normalize=True)
     return -sum(probs*np.log2(probs))
 
+# def information_gain(outcome, feature):
+#     """
+#     Your implementation assumes binary features (0 and 1):
+#     But it will break or give wrong results if:
+#     feature has more than 2 categories
+#     feature is not encoded as 0/1
+#     """
+#     # H(parent) = entropy outcome
+#     Nobs = len(outcome)
+#     H_outcome = entropy(outcome)
+#     # split outcome by feature
+#     split_left = outcome[feature == 1]
+#     split_right = outcome[feature == 0]
+#     #
+#     Nleft = len(split_left)
+#     Nright = len(split_right)
+#     #
+#     H_left = entropy(split_left)
+#     H_right = entropy(split_right)
+#     ig = H_outcome - ((Nleft/Nobs*H_left) + (Nright/Nobs*H_right))
+    
+#     return np.round(ig, 4)
+
+def information_gain(outcome, feature):
+    """
+    General Information Gain calculation for:
+    binary features
+    multi-class categorical features
+    But:
+    For continuous features, we will need thresholds (like in CART)
+    """
+    H_outcome = entropy(outcome)
+    N = len(outcome)
+    # weighted_entropy = 0
+    # for value in feature.unique():
+    #     subset = outcome[feature == value]        
+    #     weight = len(subset) / N
+    #     weighted_entropy += weight * entropy(subset)
+    
+    # pandas style avoiding looping
+    weighted_entropy = (
+        outcome.groupby(feature)
+        .apply(lambda x: (len(x)/N) * entropy(x))
+        .sum()
+    )            
+
+    return np.round(H_outcome - weighted_entropy, 4)
+
+def info_gain(parent, left, right):
+    # H(parent) = entropy outcome
+    H_parent = entropy(parent)
+    # 
+    H_left = entropy(left)
+    H_right = entropy(right)
+    return H_parent - (len(left)/len(parent)*H_left + len(right)/len(parent)*H_right)
+    return entropy(parent) - (len(left)/len(parent)*entropy(left) + len(right)/len(parent)*entropy(right))
+
 # create a dataset
 x1 = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
 x2 = [1, 1, 1, 0, 0, 0, 0, 0, 1, 1]
 x3 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
 y = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
-data = pd.DataFrame([x1, x2, x3, y])
+data = pd.DataFrame({
+    'X1': x1,
+    'X2': x2,
+    'X3': x3,
+    'Y': y
+})
 
+for i in range(data.shape[1]):
+    print(f'Entropy of {data.columns[i]} = {entropy(data[data.columns[i]])}')
+    print(f'IG for {data.columns[i]} = {information_gain(data.Y, data[data.columns[i]])}')
 
 
 
